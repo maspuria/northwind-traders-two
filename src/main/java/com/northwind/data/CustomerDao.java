37c9d4c513cc5f27acf16bbb04753ccee1d08ecb
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-03 13:36:37 -0600

    update tostring in customer and update customerdao

--- src/main/java/com/northwind/data/CustomerDao.java
+++ src/main/java/com/northwind/data/CustomerDao.java
@@ -17,124 +17,46 @@ public class CustomerDao {
         this.dataSource = dataSource;
     }
 
-    //    public returntype methodname(){
-//
-//    }
     public List<Customer> getAll() {
         List<Customer> customers = new ArrayList<>();
 
         //call the database and get back customers
-        // create Customer objects and fill them with the data form the database ResultSet
+        //create Customer objects and fill them with the data from the database ResultSet
         String query = """
                 SELECT CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax
-                FROM customers;
+                FROM Customers;
                 """;
+
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
 
-            String searchTerm = "%AIR%";
-            statement.setString(1, searchTerm);
+            try (ResultSet resultSet = statement.executeQuery()) {
 
-            try (ResultSet results = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Customer customer = new Customer(
+                            resultSet.getString("CustomerID"),
+                            resultSet.getString("CompanyName"),
+                            resultSet.getString("ContactName"),
+                            resultSet.getString("ContactTitle"),
+                            resultSet.getString("Address"),
+                            resultSet.getString("City"),
+                            resultSet.getString("Region"),
+                            resultSet.getString("PostalCode"),
+                            resultSet.getString("Country"),
+                            resultSet.getString("Phone"),
+                            resultSet.getString("Fax"));
 
-                while (results.next()) {
-                    String title = results.getString("title");
-                    String description = results.getString("description");
-                    int releaseYear = results.getInt("release_year");
-                    int length = results.getInt("length");
-
-                    System.out.println(title);
-                    System.out.println(description);
-                    System.out.println(releaseYear);
-                    System.out.println(length);
-                    System.out.println("-----------------------");
+                    customers.add(customer);
                 }
-            }
 
+            }
         } catch (SQLException e) {
             System.out.println("There was an error retrieving the data. Please try again.");
             e.printStackTrace();
         }
 
         return customers;
-
-//        // Customer 1: Alfreds Futterkiste
-//        customers.add(new Customer(
-//                "ALFKI",
-//                "Alfreds Futterkiste",
-//                "Maria Anders",
-//                "Sales Representative",
-//                "Obere Str. 57",
-//                "Berlin",
-//                null,
-//                "12209",
-//                "Germany",
-//                "030-0074321",
-//                "030-0076545"
-//        ));
-//
-//        // Customer 2: Ana Trujillo Emparedados y helados
-//        customers.add(new Customer(
-//                "ANATR",
-//                "Ana Trujillo Emparedados y helados",
-//                "Ana Trujillo",
-//                "Owner",
-//                "Avda. de la Constitución 2222",
-//                "México D.F.",
-//                null,
-//                "05021",
-//                "Mexico",
-//                "(5) 555-4729",
-//                "(5) 555-3745"
-//        ));
-//
-//        // Customer 3: Antonio Moreno Taquería
-//        customers.add(new Customer(
-//                "ANTON",
-//                "Antonio Moreno Taquería",
-//                "Antonio Moreno",
-//                "Owner",
-//                "Mataderos 2312",
-//                "México D.F.",
-//                null,
-//                "05023",
-//                "Mexico",
-//                "(5) 555-3932",
-//                null
-//        ));
-//
-//        // Customer 4: Around the Horn
-//        customers.add(new Customer(
-//                "AROUT",
-//                "Around the Horn",
-//                "Thomas Hardy",
-//                "Sales Representative",
-//                "120 Hanover Sq.",
-//                "London",
-//                null,
-//                "WA1 1DP",
-//                "UK",
-//                "(171) 555-7788",
-//                "(171) 555-6750"
-//        ));
-//
-//        // Customer 5: Berglunds snabbköp
-//        customers.add(new Customer(
-//                "BERGS",
-//                "Berglunds snabbköp",
-//                "Christina Berglund",
-//                "Order Administrator",
-//                "Berguvsvägen 8",
-//                "Luleå",
-//                null,
-//                "S-958 22",
-//                "Sweden",
-//                "0921-12 34 65",
-//                "0921-12 34 67"
-//        ));
     }
-
-
 
 }
--- src/main/java/com/northwind/model/Customer.java
+++ src/main/java/com/northwind/model/Customer.java
@@ -123,18 +123,19 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer{" +
-                "customerId='" + customerId + '\'' +
-                ", companyName='" + companyName + '\'' +
-                ", contactName='" + contactName + '\'' +
-                ", contactTitle='" + contactTitle + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", region='" + region + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", country='" + country + '\'' +
-                ", phone='" + phone + '\'' +
-                ", fax='" + fax + '\'' +
-                '}';
+        return "Customer " +
+                " customerId = " + customerId + '\n' +
+                " companyName = " + companyName + '\n' +
+                " contactName = " + contactName + '\n' +
+                " contactTitle = " + contactTitle + '\n' +
+                " address = " + address + '\n' +
+                " city = " + city + '\n' +
+                " region = " + region + '\n' +
+                " postalCode = " + postalCode + '\n' +
+                " country = " + country + '\n' +
+                " phone = " + phone + '\n' +
+                " fax = " + fax + '\n' + '\n';
     }
+
 }
+
