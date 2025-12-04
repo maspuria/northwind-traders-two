218c59e57bbde8ead311deed5323eaef1984adad
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-03 22:06:31 -0600

    update method and delete method outline commit

--- src/main/java/com/northwind/data/ProductDao.java
+++ src/main/java/com/northwind/data/ProductDao.java
@@ -94,16 +94,59 @@ public class ProductDao {
         return product;
     }
 
-    // update an existing customer
+    // update method to update an existing customer
     public void update(Product product) {
 
         String query = """
-                UPDATE
-                SET
+                UPDATE products
+                SET ProductID = ?,
+                    ProductName = ?,
+                    SupplierID = ?,
+                    CategoryID = ?,
+                    QuantityPerUnit = ?,
+                    UnitPrice = ?,
+                    UnitsInStock = ?,
+                    UnitsOnOrder = ?,
+                    ReorderLevel = ?,
+                    Discontinued = ?
                 WHERE ProductID = ?;
                 """;
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, product.getProductId());
+            statement.setString(2, product.getProductName());
+            statement.setInt(3,product.getSupplierId());
+            statement.setInt(4, product.getCategoryId());
+            statement.setString(5, product.getQuantityPerUnit());
+            statement.setDouble(6,product.getUnitPrice());
+            statement.setInt(7,product.getUnitsInStock());
+            statement.setInt(8, product.getUnitsOnOrder());
+            statement.setInt(9,product.getReorderLevel());
+            statement.setInt(10, product.getDiscontinued());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("There was an error updating the customer. Please try again.");
+            e.printStackTrace();
+        }
     }
 
+    // delete method to delete customer
+    public void delete(String productId) {
+
+        String query = """
+                DELETE FROM products
+                WHERE ProductID = ?;
+                """;
+
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query));
+
+
+
+    }
 
 
 }
